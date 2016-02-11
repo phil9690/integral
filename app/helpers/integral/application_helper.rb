79b5db406668d0b7216fdff4cde94bc042d8f7f1
Patrick Lindsay <vindicated.fool@gmail.com>
2016-02-11 17:01:33 +0900

    Add anchor_to helper

--- app/helpers/integral/application_helper.rb
+++ app/helpers/integral/application_helper.rb
@@ -2,6 +2,19 @@ module Integral
   # Base helper inherited from all Integral helpers
   module ApplicationHelper
 
+    # Creates an anchor link
+    #
+    # @param body [String] body of the link
+    # @param location [String] location of the anchor
+    #
+    # @return [String] anchor to a particular location of the current page
+    def anchor_to(body, location)
+      current_path = url_for(:only_path => false)
+      path = "#{current_path}##{location}"
+
+      link_to body, path
+    end
+
     # @return [String] markup listing flashes
     def render_flashes
       flash_types = [:notice, :alert, :error]
--- spec/helpers/integral/application_helper_spec.rb
+++ spec/helpers/integral/application_helper_spec.rb
@@ -2,6 +2,20 @@ require 'rails_helper'
 
 module Integral
   describe ApplicationHelper, type: :helper do
+
+    describe '#anchor_to' do
+      let(:body) { 'testbody' }
+      let(:location) { 'testanchor' }
+      let(:expected_path) { '/#testanchor' }
+
+      it 'generates a link_to tag with current page and supplied anchor' do
+        allow(helper).to receive(:url_for).and_return('/')
+        expect(helper).to receive(:link_to).with(body, expected_path).and_return('test/url')
+
+        expect(helper.anchor_to(body, location)).to eq 'test/url'
+      end
+    end
+
     describe '#render_flashes' do
       let(:flash) { { notice: 'Foo notice message',
         alert: 'Foo alert message',
