273796df6b9f4d3b52ce6819bb8601cb6aec6dc8
Patrick Lindsay <vindicated.fool@gmail.com>
2016-03-05 16:37:15 +0900

    Remove file_storage_type configuration as this is handled by Carrierwave

--- lib/integral/configuration.rb
+++ lib/integral/configuration.rb
@@ -1,10 +1,8 @@
 module Integral
   # Handles configurable settings within Integral
   class Configuration
-    attr_accessor :file_storage_type
 
     def initialize
-      @file_storage_type = :file
     end
   end
 end
--- spec/integral_spec.rb
+++ spec/integral_spec.rb
@@ -2,38 +2,43 @@ require "rails_helper"
 
 module Integral
   describe "#configure" do
-    let(:file_storage_type) { '' }
+    let(:foo) { '' }
 
     before do
       Integral.configure do |config|
-        config.file_storage_type = file_storage_type
+        # config.foo = foo
       end
     end
 
-    context 'file_storage_type' do
-      context 'when setting is not set' do
-        let(:file_storage_type) { '' }
-
-        it 'ImageUploader.storage_type defaults to :file' do
-          expect(ImageUploader.storage_type).to eq :file
-        end
-      end
-
-      context 'when setting is :file' do
-        let(:file_storage_type) { :file }
-
-        it 'ImageUploader.storage_type returns :file' do
-          expect(ImageUploader.storage_type).to eq :file
-        end
-      end
-
-      context 'when setting is :fog' do
-        let(:file_storage_type) { :fog }
-
-        it 'ImageUploader.storage_type returns :fog' do
-          expect(ImageUploader.storage_type).to eq :fog
-        end
-      end
+    # Keeping this spec file here as it will no doubt be used in the future when Integral is configurable
+    it 'exists' do
+      expect(true).to eq true
     end
+
+  #   context 'file_storage_type' do
+  #     context 'when setting is not set' do
+  #       let(:file_storage_type) { '' }
+  #
+  #       it 'ImageUploader.storage_type defaults to :file' do
+  #         expect(ImageUploader.storage_type).to eq :file
+  #       end
+  #     end
+  #
+  #     context 'when setting is :file' do
+  #       let(:file_storage_type) { :file }
+  #
+  #       it 'ImageUploader.storage_type returns :file' do
+  #         expect(ImageUploader.storage_type).to eq :file
+  #       end
+  #     end
+  #
+  #     context 'when setting is :fog' do
+  #       let(:file_storage_type) { :fog }
+  #
+  #       it 'ImageUploader.storage_type returns :fog' do
+  #         expect(ImageUploader.storage_type).to eq :fog
+  #       end
+  #     end
+  #   end
   end
 end
