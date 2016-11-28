45b5caea0f6a45daf76fce926b59b482b29f173a
Patrick Lindsay <vindicated.fool@gmail.com>
2016-11-28 18:21:36 +0900

    Prevent CSRF attacks by raising an exception within front and backend base controllers

--- app/controllers/integral/backend/base_controller.rb
+++ app/controllers/integral/backend/base_controller.rb
@@ -3,6 +3,10 @@ module Integral
   module Backend
     # Base controller for backend inherited by all Integral backend controllers
     class BaseController < ActionController::Base
+      # Prevent CSRF attacks by raising an exception.
+      # For APIs, you may want to use :null_session instead.
+      protect_from_forgery with: :exception
+
       layout 'integral/backend'
 
       # User authentication
